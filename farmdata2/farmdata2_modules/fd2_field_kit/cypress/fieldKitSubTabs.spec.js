5a54b3e30161e22a440105eeaa12ff693aa01426
Lukas Hamm <hamml@dickinson.edu>
2025-04-30 15:10:03 -0400

    Created test-file
    Signed-off-by: Lukas Hamm <hamml@dickinson.edu>

---
+++ farmdata2/farmdata2_modules/fd2_field_kit/cypress/fieldKitSubTabs.spec.js
@@ -0,0 +1,28 @@
+describe("Field kit sub-tabs", () => {
+	beforeEach(() => {
+		cy.login("manager1", "farmdata2")
+		cy.visit("/farm/fd2-field-kit")
+	})
+
+// Remember, try to only edit lines inside the it() you are working on :)
+
+        it("Check that the FieldKit tab contains sub-tabs for Info and Seeding Input.", () => {
+
+                
+        })
+
+
+
+        it("Check that the order of the tabs is Info and then Seeding Input.", () => {
+		
+                
+        })
+
+
+
+        it("Check that there are the correct number of sub-tabs (2 at this time).", () => {
+		
+
+        })
+
+})
